fb73f535f4a021beb8890a8321eeec24b071545e
Yadong Yu <65695568+WabbyWabbo@users.noreply.github.com>
2022-04-22 01:02:05 +0800

    create data.json when launch th app first time.

--- src/main/java/com/workflow/AdminApp.java
+++ src/main/java/com/workflow/AdminApp.java
@@ -1,12 +1,24 @@
 package com.workflow;
 
+import com.workflow.resp.RestBean;
+import com.workflow.util.FastJsonUtils;
+import com.workflow.util.FileUtil;
+import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
 
 @SpringBootApplication
 public class AdminApp {
 
+    @SneakyThrows
     public static void main(String[] args) {
         SpringApplication.run(AdminApp.class,args);
+        //运行前检查用户的data.json是否存在
+        File file = new File("data.json");
+        if (!file.exists()) {
+            FastJsonUtils.writeFile("data.json","{\"download\":{\"list\":[],\"total\":0},\"script\":{\"list\":[],\"total\":0},\"sikuli\":{\"list\":[],\"total\":0}}");
+        }
     }
 }
